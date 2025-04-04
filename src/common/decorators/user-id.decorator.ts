8da16e5c321a41cc2fe5ff300c5b5ef1e125c235
amirfarzamnia <amir2452fa@gmail.com>
2025-04-04 10:07:32 -0600

    improvements

---
+++ src/common/decorators/user-id.decorator.ts
@@ -0,0 +1,33 @@
+import type { ExecutionContext } from "@nestjs/common";
+import type { Request } from "express";
+import type { DataSource } from "typeorm";
+
+import {
+  BadRequestException,
+  createParamDecorator,
+  UnauthorizedException,
+} from "@nestjs/common";
+
+export const UserId = (dataSource: DataSource) =>
+  createParamDecorator(
+    async (_data: unknown, ctx: ExecutionContext): Promise<string> => {
+      const request = ctx.switchToHttp().getRequest<Request>();
+
+      const sessionId = request.headers["x-session-id"];
+
+      if (typeof sessionId !== "string") {
+        throw new BadRequestException("Missing header: x-session-id");
+      }
+
+      const result = await dataSource.query<{ user_id: string }[]>(
+        "SELECT user_id FROM sessions WHERE id = ?",
+        [sessionId],
+      );
+
+      if (!result.length) {
+        throw new UnauthorizedException("Invalid session");
+      }
+
+      return result[0].user_id;
+    },
+  )();
